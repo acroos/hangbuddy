7d96cf52bfa26d8450963ef22fd35012b9899d3a
Austin C Roos <austin.roos@wetransfer.com>
2021-12-29 22:57:09 +0100

    Remove unused code

--- lib/entities/repetition_routine.dart
+++ lib/entities/repetition_routine.dart
@@ -21,7 +21,7 @@ class RepetitionRoutine {
     return "${minutes}m${seconds}s";
   }
 
-  Map<String, Object> toJson() {
+  Map<String, dynamic> toJSONEncodable() {
     return {
       'name': name,
       'hangTime': hangTime,
@@ -30,21 +30,11 @@ class RepetitionRoutine {
     };
   }
 
-  Map<String, dynamic> toJSONEncodable() {
-    return toJson();
-  }
-
   factory RepetitionRoutine.fromJson(Map<String, dynamic> json) {
     return RepetitionRoutine(
         name: json['name'],
         hangTime: json['hangTime'],
         restTime: json['restTime'],
         repetitions: json['repetitions']);
-  }
-
-  static List<RepetitionRoutine> fromJsonList(List<dynamic> list) {
-    return list.cast<Map<String, dynamic>>().map((item) {
-      return RepetitionRoutine.fromJson(item);
-    }).toList();
   }
 }
